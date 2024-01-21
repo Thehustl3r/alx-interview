ed0f14d439c90adc3401c3384e21735a39cd281c
Prosper Mugisha <mprosp3r@gmail.com>
2024-01-21 13:56:36 +0200

    0x02. Minimum Operations Algorithm

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,29 @@
+#!/usr/bin/pyhton3
+"""0-minioperation"""
+
+
+from queue import Empty
+
+
+def multiplesOfNum(n):
+    mutlipes = []
+    multi = 2
+    while (n > 1):
+        while (n % multi != 0):
+            multi = multi+1
+        mutlipes.append(multi)
+        n = n / multi
+
+    return mutlipes
+
+
+def minOperations(n) -> int:
+    """function that return minimal operation to create n times of H"""
+    H = 1
+    multiples = multiplesOfNum(n)
+    num: int = 0
+    while (len(multiples)):
+        multi: int = multiples.pop()
+        num = num + multi
+
+    return num
