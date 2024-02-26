11618b0f81cd6b971156c50fd1f28ca529f23e60
Prosper Mugisha <mprosp3r@gmail.com>
2024-02-26 03:47:33 +0200

    my first commit

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""rotate_2d_matrix"""
+
+
+def rotate_2d_matrix(matrix):
+    """
+      Rotate The 2 matrix
+      Args:
+          - matrix: list of two dimension array
+      Return:
+          - Nothing
+      """
+    # Get the number of rows and columns
+    n = len(matrix)
+
+    # 1. Transpose the matrix
+    for i in range(n):
+        for j in range(i + 1, n):  # Avoid redundant swaps (i == j)
+            # Swap elements
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    # 2. Reverse each row
+    for row in matrix:
+        row.reverse()
