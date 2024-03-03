dfa27ad2f12ce6dc801f85f0b78caca5385ead2d
Prosper Mugisha <mprosp3r@gmail.com>
2024-03-04 00:04:29 +0200

    first commit

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+0-making_change module
+"""
+
+
+def makeChange(coins, total):
+    """
+        The Program that find min number to reach target amount.
+        Args:
+            - coins: the list of available coins
+            - total: the targeted amount to reach
+        Return:
+            - the minimun coins, otherwise -1
+    """
+    number = 0
+    prev_total = total
+    coins = sorted(coins, reverse=True)
+    while total > 0:
+        prev_total = total
+        for coin in coins:
+            if (total - coin) >= 0:
+                total -= coin
+                number += 1
+                break
+        if total == prev_total:
+            break
+    if number == 0 or total != 0:
+        return -1
+    return number
